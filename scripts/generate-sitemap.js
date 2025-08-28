b31b7ae533ea155102928c20dfa6953c29472082
Fatih Yıldız <fatihyildiz@Mac.home>
2025-08-28 12:41:55 +0300

    Auto-deploy

--- scripts/generate-sitemap.js
+++ scripts/generate-sitemap.js
@@ -18,7 +18,7 @@ function generateSitemap() {
   const currentDate = new Date().toISOString().split('T')[0];
   
   const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
-<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+<urlset xmlns="https://www.sitemaps.org/schemas/sitemap/0.9">
   <!-- Ana Sayfa -->
   <url>
     <loc>${baseUrl}/</loc>
